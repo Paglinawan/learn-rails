459eb8805cf7131467fdb917a0825b7a0990e2f7
miho <32391651+Paglinawan@users.noreply.github.com>
2024-03-10 10:29:50 +0000

    Learn: tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "hello#index"
+  get "/posts", to: "posts#index", as: "posts" # posts_path
+  get "/posts/:id", to: "posts#show", as: "post" # post_path()
 end
