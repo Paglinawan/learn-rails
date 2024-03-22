f3ee52828eb0999cbcd467c8768b037e6ad476e9
miho <32391651+Paglinawan@users.noreply.github.com>
2024-03-22 11:56:53 +0000

    Update: create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,14 @@ class PostsController < ApplicationController
 
   def create
     # ↓ Active Record
-    Post.create(title: params[:post][:title], body: params[:post][:body])
+    Post.create(post_params)
 
     redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
