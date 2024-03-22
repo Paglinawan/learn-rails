8acee948863a10e60f97f5a4761b51ce27e515f0
miho <32391651+Paglinawan@users.noreply.github.com>
2024-03-22 11:31:38 +0000

    Change: root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: "posts#index", as: "posts" # posts_path
-  get "/posts/:id", to: "posts#show", as: "post" # post_path()
+  root "posts#index"
+
+  resources :posts
 end
